ce61cd8e4c7d71baf684d44a489d9b4bf555f137
Wcapers <Mirazizovt@yandex.ru>
2020-11-30 21:23:37 +0300

    add cart model

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Cart, type: :model do
+  it { should have_many(:cart_items) }
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:user_id) }
+end
